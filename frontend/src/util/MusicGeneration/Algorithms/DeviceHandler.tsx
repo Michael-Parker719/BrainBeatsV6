a2d75b93ba5fccea6c59f01531bf0865f5f20fcf
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-11-14 16:34:43 -0500

    Update DeviceHandler.tsx
    trying arduino reading

--- frontend/src/util/MusicGeneration/Algorithms/DeviceHandler.tsx
+++ frontend/src/util/MusicGeneration/Algorithms/DeviceHandler.tsx
@@ -1,18 +1,59 @@
 /* This code will obtain data from the device and pass it off to the music generation
- as a single line array. Link: https://github.com/dbuezas/arduino-web-uploader */
+ as a single line array. 
+ Arduino Uploader Library: https://github.com/dbuezas/arduino-web-uploader 
+ Web Serial Port Library: https://github.com/WICG/serial
+ */
 
 import { upload, boards } from 'web-arduino-uploader'
+import { SerialPort } from 'w3c-web-serial'
 
+export abstract class DeviceHandler
+{
+
+ // change below to a public function
 document.addEventListener('button', async () => {
   const onProgress = (percentage) => {
     console.log(percentage + '%')
   }
  
-  //const verify = false // optional
-  //const portFilters = {} // optional, e.g. [{"usbProductId":46388,"usbVendorId":1241}]
+  const verify = false // optional
+  const portFilters = {} // optional, e.g. [{"usbProductId":46388,"usbVendorId":1241}]
   console.log('starting')
-  //https://brainbeatz.xyz/frontend/src/util/MusicGeneration/Algorithms/FILENAME
+ 
+  // https://brainbeatz.xyz/frontend/src/util/MusicGeneration/Algorithms/FILENAME
   await upload(boards.nanoOldBootloader, 'http://your-site.com/hex-file.hex', onProgress, verify, portFilters)
+  
   console.log('done!')
 })
 
+// Listen to the serial port
+ public async listen(){
+  // read setup
+  const filter = {};
+  const port = await navigator.serial.requestPort({ filters: [filter] });
+  await port.open({baudRate: 9600})
+  
+  // reading from port
+  while (port.readable) {
+   const reader = port.readable.getReader();
+   try {
+     while (true) {
+       const { value, done } = await reader.read();
+      console.log(value)
+       if (done) {
+         // |reader| has been canceled.
+         break;
+       }
+       // Do something with |value|...
+     }
+   } catch (error) {
+     // Handle |error|...
+     console.log("error")
+   } finally {
+     reader.releaseLock();
+   }
+}
+
+  }
+ 
+}
