c8dd4b6148bb85741857d25a2ed1c903e34ec5d9
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-11-15 16:45:51 -0500

    Delete frontend/src/Pages/DeviceTestPage.tsx
    wasnt for main

--- frontend/src/Pages/DeviceTestPage.tsx
+++
@@ -1,8 +0,0 @@
-const DeviceTestPage = () => {
-  return (
-    <body>
-        <h1> This is a testing page</h1>
-        <input type="button" value="Click" id="coolbutton"></input>
-    </body>)
-
-}
