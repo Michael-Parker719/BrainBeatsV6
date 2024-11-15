8859c93febef766e9f8033b85dd97f3f70f74bd4
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-11-15 11:49:48 -0500

    Update DeviceHandlerTest.tsx

--- frontend/src/util/DeviceHandlerTest.tsx
+++ frontend/src/util/DeviceHandlerTest.tsx
@@ -1,1 +1,16 @@
-import 
+import { DeviceHandler } from './DeviceHandler';
+
+const device = new DeviceHandler();
+
+export async function testRun(){
+  // setting file path for hex
+  device.setHexFilePath("path");
+
+  // upload arduino code
+  await device.upload()
+
+  // listen to serial output
+  device.listen()
+} 
+
+
