41e78b29d419dfb1b5a060b888b7549534936396
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2025-01-06 17:37:10 -0500

    Update DeviceTestPage.tsx

--- frontend/src/Pages/DeviceTestPage.tsx
+++ frontend/src/Pages/DeviceTestPage.tsx
@@ -4,7 +4,7 @@ const DeviceTestPage = () => {
 
       <h1> This is a Testing Page</h1>
 
-      <input type="button" onClick = { () => alert('Uploading Script') } value="Click" id="coolbutton"></input>
+      <input type="button" onClick = { () => alert('Uploading Script') } value="click" id="coolbutton"></input>
       
     </body>
   )
