52a5b6f84a22de5dfaff10d5939a9a9445761848
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2025-01-08 15:06:20 -0500

    Update DeviceTestPage.tsx

--- frontend/src/Pages/DeviceTestPage.tsx
+++ frontend/src/Pages/DeviceTestPage.tsx
@@ -1,10 +1,35 @@
+import React, { MouseEvent } from 'react';
+import { DeviceHandler } from "../util/DeviceHandler";
+
+// Page for testing the arduino Uno
 const DeviceTestPage = () => {
+  const device = new DeviceHandler();
+
+  // Handles button (source: https://felixgerschau.com/react-typescript-onclick-event-type/)
+  // Performs a function onClick
+  const onClickFunc = (e:MouseEvent<HTMLButtonElement>) => {
+    // setting file path for hex
+    console.log('Setting Hex Path');
+    //device.setHexFilePath("./MindControl3/build/arduino.avr.uno/MindControl3.ino.hex");
+
+    // upload arduino code
+    console.log('Uploading Script');
+    //device.uploadToArduino();
+
+    // listen to serial output
+    alert('Listening to Serial Port')
+    //device.listen();
+    console.log("fin");
+  }
+
   return (
     <body>
 
       <h1> This is a Testing Page</h1>
 
       <input type="button" onClick = { () => alert('Uploading Script') } value="click" id="coolbutton"></input>
+      
+      <button onClick={onClickFunc}>Button #2</button>
       
     </body>
   )
