524ea43d2660ee8a3378a42880a4afb109dc372a
Kensley <ke049038@SillyComputer>
2025-02-25 23:13:08 -0500

    Added in more merged changes

--- frontend/src/util/MusicGeneration/Algorithms/AbstractNoteHandler.tsx
+++ frontend/src/util/MusicGeneration/Algorithms/AbstractNoteHandler.tsx
@@ -1,46 +1,13 @@
-<<<<<<< HEAD
-import { DataStream4Ch, DataStream8Ch, MusicSettings } from "../../Interfaces";
-=======
 import {
   DataStream4Ch,
   DataStream8Ch,
   DataStream5Waves,
   MusicSettings,
 } from "../../Interfaces";
->>>>>>> main
 import { TDebugOptionsObject } from "../../Types";
 
 /* This abstract class contains all named functions that must be implemented in your new notehandler class
 for any new music generation algorithm.*/
-<<<<<<< HEAD
-export abstract class AbstractNoteHandler 
-{
-
-    protected debugOutput:boolean;
-    protected stopFlag:boolean = false;
-
-    constructor(settings:MusicSettings, debugOptionsObject: TDebugOptionsObject)
-    {
-        this.debugOutput = debugOptionsObject.debugOption2;
-    }
-
-    public abstract originalNoteGeneration(EEGdataObj: DataStream8Ch|DataStream4Ch) : any;
-
-    public abstract returnMIDI(): Promise<Uint8Array>;
-
-    public setDebugOutput(b: boolean)
-    {
-        this.debugOutput = b;
-        if (this.debugOutput) console.log("Setting Notehandler debug to ", b);
-    }
-
-    public setStopFlag()
-    {
-        this.stopFlag = true;
-    }
-
-}
-=======
 export abstract class AbstractNoteHandler {
   protected debugOutput: boolean;
   protected stopFlag: boolean = false;
@@ -67,4 +34,3 @@ export abstract class AbstractNoteHandler {
     this.stopFlag = true;
   }
 }
->>>>>>> main
