891db1c3b878f3b8395873f4983e1a80a26fc94d
Alzander245 <superflamer4@gmail.com>
2025-04-03 23:24:37 -0400

    Hardcoded Random Music settings to only piano

--- frontend/src/util/MusicGeneration/MusicHelperFunctions.tsx
+++ frontend/src/util/MusicGeneration/MusicHelperFunctions.tsx
@@ -132,7 +132,7 @@ function getRandomArbitrary(min:number, max:number) {
   
 // First to last, -3 (Sinewave) 8 + 1(Piano)
 export function getRandomInstrument() {
-    return getRandomArbitrary(1, 2);
+    return 1.0;//getRandomArbitrary(0, 1);
 }
 
 // Frist to last, 0 (Whole) 4 (Sixteenth)
